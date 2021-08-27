0f63a21d863203d8748ab134d4db1e538fb48a37
politrons <politrons@gmail.com>
2021-08-27 18:32:15 +0200

    Add new module with Kafka

--- StarWarsShips/src/main/java/com/politrons/app/StarWarsShipsApp.java
+++ StarWarsShips/src/main/java/com/politrons/app/StarWarsShipsApp.java
@@ -2,13 +2,20 @@ package com.politrons.app;
 
 import akka.actor.ActorSystem;
 import akka.kafka.ConsumerSettings;
+import akka.kafka.ProducerSettings;
 import akka.kafka.Subscriptions;
 import akka.kafka.javadsl.Consumer;
+import akka.kafka.javadsl.Producer;
 import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import io.vavr.control.Try;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -18,6 +25,11 @@ import static io.vavr.API.println;
 public class StarWarsShipsApp {
 
     private static final ActorSystem system = ActorSystem.create("StarWarsShipsApp");
+
+    static Map<String, String> ships = Map.of(
+            "episode1", "Falcon",
+            "episode2", "Tie",
+            "episode3", "Destroyer");
 
     public static void main(String[] args) {
         kafkaConsumer();
@@ -37,14 +49,30 @@ public class StarWarsShipsApp {
                 .mapAsync(
                         1,
                         msg -> {
-                            String message = new String(msg.record().value());
-                            println("Message from Kafka:" + message);
-                            return CompletableFuture.completedFuture(message)
+                            String episode = new String(msg.record().value());
+                            println("Request episode from client:" + episode);
+                            kafkaPublisher(ships.getOrDefault(episode,"No ships info"));
+                            return CompletableFuture.completedFuture(episode)
                                     .thenApply(done -> msg.committableOffset());
                         }
 
                 )
                 .to(Sink.ignore())
                 .run(system);
+    }
+
+    private static void kafkaPublisher(String message) {
+        final var producerConfig = system.settings().config().getConfig("akka.kafka.producer");
+        final var producerSettings =
+                ProducerSettings.create(producerConfig, new StringSerializer(), new StringSerializer())
+                        .withBootstrapServers("localhost:9092");
+
+        var publisherResult =
+                Source.single(new ProducerRecord<String, String>("starWarsShipsResponse", message))
+                        .runWith(Producer.plainSink(producerSettings), system);
+        var messageSent = Try.of(() -> publisherResult.toCompletableFuture().get());
+        if(messageSent.isFailure()) {
+            println("Error sending back message to Kafka. Caused by " + messageSent.getCause());
+        }
     }
 }
--- starWarsMovies/src/main/java/com/politrons/infra/ShipsConnector.java
+++ starWarsMovies/src/main/java/com/politrons/infra/ShipsConnector.java
@@ -2,14 +2,24 @@ package com.politrons.infra;
 
 import akka.Done;
 import akka.actor.ActorSystem;
+import akka.kafka.ConsumerMessage;
+import akka.kafka.ConsumerSettings;
 import akka.kafka.ProducerSettings;
+import akka.kafka.Subscriptions;
+import akka.kafka.javadsl.Consumer;
 import akka.kafka.javadsl.Producer;
+import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
 import io.vavr.concurrent.Future;
+import io.vavr.concurrent.Promise;
 import io.vavr.control.Try;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
 import static io.vavr.API.*;
@@ -24,7 +34,11 @@ public class ShipsConnector {
     private final ActorSystem system = ActorSystem.create("StarWarsShipsApp");
 
     public Future<String> getShips(String episode) {
+        sendRequest(episode);
+        return getShipsSubscriber();
+    }
 
+    private void sendRequest(String episode) {
         final var producerConfig = system.settings().config().getConfig("akka.kafka.producer");
         final var producerSettings =
                 ProducerSettings.create(producerConfig, new StringSerializer(), new StringSerializer())
@@ -38,8 +52,38 @@ public class ShipsConnector {
                 Case($Success($()), processSuccessChannel()),
                 Case($Failure($()), processFailureChannel())
         );
+    }
 
-        return Future.successful("Falcon");
+    private Future<String> getShipsSubscriber() {
+        var promise = Promise.<String>make();
+        final var config = system.settings().config().getConfig("akka.kafka.consumer");
+        final var consumerSettings =
+                ConsumerSettings.create(config, new StringDeserializer(), new ByteArrayDeserializer())
+                        .withBootstrapServers("localhost:9092")
+                        .withGroupId("starWarsShipsGroupId")
+                        .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+                        .withProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
+                        .withProperty(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "5000");
+
+        Consumer.committableSource(consumerSettings, Subscriptions.topics("starWarsShipsResponse"))
+                .mapAsync(
+                        1,
+                        msg -> {
+                            String message = new String(msg.record().value());
+                            println("Episode ships response from Kafka:" + message);
+                            promise.success(message);
+                            return ackMessage(msg, message);
+                        }
+
+                )
+                .to(Sink.ignore())
+                .run(system);
+        return promise.future();
+    }
+
+    private CompletableFuture<ConsumerMessage.CommittableOffset> ackMessage(ConsumerMessage.CommittableMessage<String, byte[]> msg, String message) {
+        return CompletableFuture.completedFuture(message)
+                .thenApply(done -> msg.committableOffset());
     }
 
     private Function<Done, Done> processSuccessChannel() {
